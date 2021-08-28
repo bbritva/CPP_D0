d00d1981e21b49a0c619d9d9b12dc97190fa2db1
grvelva <grvelva@student.21-school.ru>
2021-08-28 12:29:59 +0300

    ex00 improved

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,29 +1,28 @@
 #include <iostream>
 
-int scream(char *str)
+void scream(char *str)
 {
-	int			i;
+	int	i;
 
 	i = -1;
 	while(str[++i])
-	{
-		if (str[i] >= 'a' && str[i] <= 'z')
-			std::cout << static_cast<char>(str[i] - ('a' - 'A'));
-		else
+		(str[i] >= 'a' && str[i] <= 'z') ?
+			std::cout << static_cast<char>(std::toupper(str[i])) :
 			std::cout << str[i];
-	}
-	return (0);
 }
 
 int main(int argc, char *argv[])
 {
 	int i;
-	
+
 	i = 0;
-	while (++i < argc)
-		scream(argv[i]);
 	if (argc == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else
+		while (++i < argc)
+		{
+			scream(argv[i]);
+		}
 	std::cout << std::endl;
 	return 0;
 }
